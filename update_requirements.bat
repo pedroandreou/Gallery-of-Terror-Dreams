0df573c0921bf7f6e40be8a792df4f8b5ea9cb83
Petros <pandreou01@gmail.com>
2023-02-26 02:35:13 +0000

    Update bat file

--- update_requirements.bat
+++ update_requirements.bat
@@ -5,5 +5,5 @@ REM Install packages from unpinned_requirements.txt
 pip install -r unpinned_requirements.txt
 
 REM Create a new requirements.txt file with pinned dependencies
-echo "# Created automatically by update_requirements.bat. Do not update manually!" > requirements.txt
+echo # Created automatically by update_requirements.bat. Do not update manually! > requirements.txt
 pip freeze | findstr /V "pkg_resources" >> requirements.txt
