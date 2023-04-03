b91fb1d0d2e5352a8632291052a9dc18a2efc2a3
pedroandreou <pandreou01@gmail.com>
2023-04-03 01:04:41 +0100

    Update persmissions for nltk dir & update gpt3 api with chatgpt

--- src/back-end/gpt3/gpt3.py
+++ src/back-end/gpt3/gpt3.py
@@ -1,35 +1,44 @@
-import re
 from typing import List
 
 import openai
 
 
 def generate_bullet_points_using_gpt3(text: str, openai_key: str):
-    def generate_output_dictionary(input_str: str) -> List[dict]:
-        output_list = [
-            re.sub(r"^[0-9]+\. ", "", s.strip()) for s in input_str.split("\n") if s
-        ]
+    def generate_output_dictionary(text: str) -> List[str]:
+        # Remove leading whitespaces
+        lines = [line.lstrip() for line in text.splitlines()]
+
+        # Get the text after the first whitespace
+        # e.g. "1. Hi there" => "Hi there"
+        text_list = [line.split(" ", 1)[1] for line in lines]
 
         bullet_dict = [
             {"number": i + 1, "sentence": sentence}
-            for i, sentence in enumerate(output_list)
+            for i, sentence in enumerate(text_list)
         ]
 
         return bullet_dict
 
-    prompt = f"Generate five bullet points to narrate a horror story of: {text}"
+    prompt = f"Generate five sentences to narrate a horror story of: {text}\n1."
 
     openai.api_key = openai_key
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1000,
-        n=2,
-        stop=None,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        temperature=0.6,
+        max_tokens=256,
         top_p=1,
-        temperature=0.4,
+        frequency_penalty=0.72,
+        presence_penalty=0.72,
     )
 
-    bullet_dict = generate_output_dictionary(response.choices[0].text.lstrip())
+    output = "1. " + response.choices[0].message.content.lstrip()
 
-    return bullet_dict
+    bullet_points = generate_output_dictionary(output)
+
+    return bullet_points
